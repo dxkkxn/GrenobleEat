da985e553c4721c84b967b1c9ae61ef9cd46842c
Etienne Langlet <etienne.langlet@grenoble-inp.org>
2022-12-01 14:50:11 +0100

    debut d'UI qui ressemble a qqchose

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,91 @@ import org.jline.reader.impl.completer.StringsCompleter;
 
 public class Main
 {
+    private static String prompt = "GrenobleEat>";
+    private static StringsCompleter s = new StringsCompleter("parcourir_categories", "abcdef", "connect", "passer_commande", "disconnect");
+    private static LineReader reader = LineReaderBuilder.builder().completer(s).build();
+
     public static void main(String[] args) {
-        StringsCompleter s = new StringsCompleter("parcourir_categories", "abcdef");
-        LineReader reader = LineReaderBuilder.builder().completer(s).build();
-        String prompt = "GrenobleEat>";
         while (true) {
             String line = null;
             try {
                 line = reader.readLine(prompt);
-                System.out.println(line);
                 ParsedLine pl = reader.getParser().parse(line, 0);
                 //pl.words().subList(1, pl.words().size()).toArray(new String[0]);
-                System.out.println(pl.words());
+                //System.out.println(pl.words());
+
+                //1 mot par line, pour des commandes simples
+                if(pl.words().size() == 1){
+                    switch(pl.words().get(0)){
+                        case "connect":
+                            userConnect();
+                            break;
+                        case "parcourir_categories":
+                            //TODO
+                            //je suppose que cette fonctionnalité est dispo quand on n'est pas connecté ?
+                            break;
+
+                        default:
+                            printHelpNotConnected();
+                            break;
+                    }
+                }
             } catch (UserInterruptException e) {
                 // Ignore
             } catch (EndOfFileException e) {
                 return;
+            }
+        }
+
+    }
+    static private void printHelpNotConnected(){
+        System.out.println("Commandes disponibles : \n\t - connect\n\t - parcourir_categories");
+    }
+
+    static private void printHelpConnected(){
+        System.out.println("Commandes disponibles : \n\t - disconnect\n\t - parcourir_categories\n\t - passer_commande");
+    }
+
+    static private void userConnect(){
+        String promptUser = prompt + " mail :";
+        String promptPwd = prompt + " password :";
+        String line = null;
+        boolean connectionOk = false;
+        while (!connectionOk){
+            ParsedLine pl = null;
+            while(pl == null || pl.words().size() != 1){
+                line = reader.readLine(promptUser);
+                pl = reader.getParser().parse(line, 0);
+            }
+            ParsedLine pl2 = null;
+            while(pl2 == null || pl2.words().size() != 1){
+                line = reader.readLine(promptPwd);
+                pl2 = reader.getParser().parse(line, 0);
+            }
+            //co = essaiConnection();
+            connectionOk = true; 
+            System.out.println("mail : " + pl.words().get(0) + "\npassword : " + pl2.words().get(0));
+        }
+        System.out.println("connectionOk");
+
+        //ici, l'user est connecté
+        while(true){
+            //TODO : get username et l'afficher
+            String promptConnected = "Welcome To "+ prompt; 
+            line = reader.readLine(promptConnected);
+            ParsedLine pl = reader.getParser().parse(line, 0);
+            //1 mot par line, pour des commandes simples
+            if(pl.words().size() == 1){
+                switch(pl.words().get(0)){
+                    case "disconnect":
+                        return ;
+                    case "passer_commande":
+                        //TODO
+                        break;
+                    default:
+                        printHelpConnected();
+                        break;
+                }
             }
         }
 
