978d7feeb1ea7b98da2a1f9506ae35d7eaeffdda
Etienne Langlet <etienne.langlet@grenoble-inp.org>
2022-12-07 16:44:32 +0100

    connection ok, commande pas testée

---
+++ src/App.java
@@ -0,0 +1,307 @@
+import java.sql.*;
+import java.util.List;
+import java.time.*;
+import java.time.format.*;
+
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.ParsedLine;
+import org.jline.reader.UserInterruptException;
+import org.jline.reader.impl.completer.StringsCompleter;
+
+public class App
+{
+    private String prompt;
+    private DelegateCompleter s = new DelegateCompleter();
+    private LineReader reader = LineReaderBuilder.builder().completer(s).build();
+    private boolean quit = false;
+    private boolean quitBrowseMode = true;
+    private boolean connected = false;
+    private boolean browsingCategories = false;
+    private Connection connection;
+    private Client c;
+
+    public void main() {
+        connectDatabase();
+        System.out.println("Welcome to GrenobleEat");
+        printHelpNotConnected();
+        prompt = "GrenobleEat (Not connected)> ";
+
+        s.setCompleter(new StringsCompleter("connect",
+                                            "quit"));
+        ParsedLine pl = null;
+        String line = null;
+        while (!quit) {
+            try {
+                line = reader.readLine(prompt);
+                pl = reader.getParser().parse(line, 0);
+            } catch (UserInterruptException e) {
+                // Ignore
+            } catch (EndOfFileException e) {
+                return;
+            }
+            if (pl.words().size() != 1) {
+                printHelpNotConnected();
+            } else {
+                if (connected) {
+                    switchConnected(pl.words().get(0));
+                } else {
+                    switchNotConnected(pl.words().get(0));
+                }
+                }
+            }
+    }
+
+    void connectDatabase() {
+        try {
+            // Load the JDBC driver
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Driver loaded");
+            // Try to connect
+            connection = DriverManager.getConnection
+            ("jdbc:mariadb://localhost/GrenobleEat", "etienne", "pwd");
+            System.out.println("Connected succesfully to database");
+            Client.setConnection(connection);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    void switchBrowsingCategories() {
+
+    }
+    void switchNotConnected(String cmd) {
+        switch(cmd){
+            case "connect":
+                userConnect();
+                break;
+            case "quit":
+                disconnectDatabase();
+                quit = true;
+                break;
+            default:
+                printHelpNotConnected();
+                break;
+        }
+    }
+    void disconnectDatabase() {
+        try {
+            connection.close();
+            System.out.println("Disconnected succesfully from database");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+    void switchConnected(String cmd) {
+        switch(cmd){
+            case "disconnect":
+                userDisconnect();
+                //disconnectDatabase();
+                break;
+            case "browseCategories":
+                quitBrowseMode = false;
+                browseCategories();
+                break;
+            case "passerCommande":
+                placeOrder();
+                break;
+            case "quit":
+                disconnectDatabase();
+                quit = true;
+                break;
+            default:
+                printHelpConnected();
+                break;
+        }
+    }
+
+    private void placeOrder(){
+        try{
+            PreparedStatement save = null;
+            save = connection.prepareStatement("SAVEPOINT avantCommande");
+            save.executeQuery();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return;
+        }
+        prompt = "GrenobleEat >\nChoisissez un restaurant > ";
+        boolean restaurantChoisi = false;
+        String line;
+        ParsedLine pl = null;
+        while(!restaurantChoisi){
+            line = reader.readLine(prompt);
+            pl = reader.getParser().parse(line, 0);
+            if(pl.words().size() == 1){
+                restaurantChoisi = Client.checkRestau(pl.words().get(0));
+            }
+        }
+        String nomRestaurant = pl.words().get(0);
+        String mailRestaurant = Client.getMailRestaurant(nomRestaurant);
+        LocalDateTime now = LocalDateTime.now();
+        String date = getDate(now);
+        String heure = getHeure(now);
+        prompt = "GrenobleEat >\nChoisissez un / des plats suivi d'une quantité, puis entrez 'ok' > ";
+        int prix = 0;
+        while (true) {
+            line = reader.readLine(prompt);
+            pl = reader.getParser().parse(line, 0);
+            if(pl.words().size() == 2){
+                String nomPlat = pl.words().get(0);
+                if(nomPlat.equals("ok")) break;
+                int idPlat = Client.checkRestauAPlat(mailRestaurant, nomPlat);
+                if(idPlat != 0){
+                    int quantite = Integer.parseInt(pl.words().get(1));
+                    prix += quantite * Client.getPrix(mailRestaurant, idPlat);
+                    Client.ajoutePlat(date, heure, mailRestaurant, idPlat, quantite);
+                }
+                else{
+                    System.out.println("Plat non trouvé");
+                }
+            }
+        }
+        Client.ajouteCommande(date, heure, mailRestaurant, prix);
+        prompt = "GrenobleEat >";
+    }
+
+    private String getDate(LocalDateTime now){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return dtf.format(now);
+    }
+
+    private String getHeure(LocalDateTime now){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return dtf.format(now);
+    }
+
+    private void printInfoBrowseCat(String currentCat) {
+        System.out.println("Current category: " + currentCat);
+        System.out.println("Available subcategories : ");
+        for (String cat: c.getSubcategories(currentCat)) {
+            System.out.println("\t"+cat);
+        }
+        List<String> completion = c.getSubcategories(currentCat);
+        completion.add("printRestaurants");
+        completion.add("quitBrowseMode");
+        completion.add("quit");
+        s.setCompleter(new StringsCompleter(completion));
+
+
+    }
+    private String concatWords(List<String> ls) {
+        String res = "";
+        for (int i = 0; i < ls.size()-1; i++) {
+            res += ls.get(i);
+            res += " ";
+        }
+        res += ls.get(ls.size()-1);
+        return res;
+    }
+    private void browseCategories() {
+        String currentCat = c.getRoot();
+        printInfoBrowseCat(currentCat);
+        String cmd = " ";
+        ParsedLine pl = null;
+        while (!quitBrowseMode) {
+            try {
+                String line = reader.readLine(prompt);
+                pl = reader.getParser().parse(line, 0);
+                cmd = concatWords(pl.words());
+
+            } catch (UserInterruptException e) {
+                // Ignore
+            } catch (EndOfFileException e) {
+                return;
+            }
+            if (c.getSubcategories(currentCat).contains(cmd)) {
+                currentCat = cmd;
+                System.out.println();
+                printInfoBrowseCat(currentCat);
+            } else if (pl.words().get(0).equals("print")) {
+                c.printRestaurant(pl.words().get(1));
+            } else {
+                switch(cmd){
+                    case "printRestaurants":
+                        c.printRestaurants(currentCat);
+                        break;
+                    case "quitBrowseMode":
+                        System.out.println("Quiting browsing categories mode");
+                        s.setCompleter(new StringsCompleter("browseCategories", "connect",
+                                                            "passer_commande", "disconnect",
+                                                            "quit"));
+                        quitBrowseMode = true;
+                        break;
+                    case "quit":
+                        disconnectDatabase();
+                        quitBrowseMode = true;
+                        quit = true;
+                        break;
+                    default:
+                        printHelpBrowseCat();
+                        break;
+                }
+            }
+        }
+
+    }
+
+    private void printRestaurants(List<String> l) {
+        for (String s : l) {
+            System.out.println(s);
+        }
+    }
+
+
+    private void printHelpBrowseCat(){
+        System.out.println("Commandes disponibles : \n"
+                           +"\t - $(SubCategory) : to change of category\n"
+                           +"\t - printRestaurants : to print restaurants of current category"
+                           +"\t - quitBrowseMode : to quit browsing categories mode"
+                           +"\t - quit");
+    }
+    private void printHelpNotConnected(){
+        System.out.println("Commandes disponibles : \n\t - connect\n\t - quit");
+    }
+    private void printHelpConnected(){
+        System.out.println("Commandes disponibles : \n\t - disconnect\n\t - browseCategories\n\t - passerCommande");
+    }
+    private void userDisconnect() {
+        connected = false;
+        prompt = "GrenobleEat (Not connected)> " ;
+        s.setCompleter(new StringsCompleter("browseCategories", "connect",
+                                            "quit"));
+    }
+    private void userConnect(){
+        c = new Client(connection);
+        String promptUser = "Please enter you email : ";
+        String promptPwd = "Please enter you password : ";
+        String line = null;
+        boolean connectionOk = false;
+        ParsedLine pl = null;
+        while (!connectionOk){
+            
+            while(pl == null || pl.words().size() != 1 || pl.words().get(0).equals("")){
+                line = reader.readLine(promptUser);
+                pl = reader.getParser().parse(line, 0);
+            }
+            ParsedLine pl2 = null;
+            while(pl2 == null || pl2.words().size() != 1 || pl2.words().get(0).equals("")){
+                line = reader.readLine(promptPwd);
+                pl2 = reader.getParser().parse(line, 0);
+            }
+            connectionOk = Client.essaiConnection(pl.words().get(0), pl2.words().get(0));
+        }
+        Client.setIdUser(pl.words().get(0));
+        System.out.println("connection OK");
+        connected = true; //redondant avec connectioOk ?
+        // updatePrompt
+        prompt = "GrenobleEat> " ;
+        s.setCompleter(new StringsCompleter("browseCategories",
+                                            "passerCommande", "disconnect",
+                                            "quit"));
+        // creer une instance de client avec les donnees recuperees.
+    }
+
+}
--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,321 @@
-import java.jdbc.*;
 
-public static class Client{
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-    private static PreparedStatement getPwdStm = "SELECT motDePasse FROM Client WHERE mailClient LIKE ?";
+public class Client {
+    private static Connection conn;
+    private static PreparedStatement subcategoriesStmt;
+    private static PreparedStatement resInCatStmt;
+    private static PreparedStatement categoriesOfResStmt;
+    private static PreparedStatement infoResStmt;
+    private static PreparedStatement scheduleDayStmt;
+    private static PreparedStatement getPwdStmt;
+    private static PreparedStatement getAllRestauStmt;
+    private static PreparedStatement checkRestauAPlatStmt;
+    private static PreparedStatement getIdUser;
+    private static PreparedStatement ajoutePlatStmt;
+    private static PreparedStatement getPrixStmt;
+    private static PreparedStatement getMailRestaurantStmt;
+    private static PreparedStatement ajouteCommandeStmt;
+    private static PreparedStatement getIdUserStmt;
 
-    public static boolean essaiConnection(String mail, String password){
-        PreparedStatement final = getPwdStm;
-        final.setString(1, mail);
-        final.prepareStatement();
+    private static int idUser;
+
+    public static void setConnection(Connection connection){
+        conn = connection;
+    }
+
+    public void prepareAllStatements() {
+        try {
+            getMailRestaurantStmt = conn.prepareStatement("SELECT mailRestaurant FROM Restaurant WHERE nomRestaurant LIKE ?");
+            ajouteCommandeStmt = conn.prepareStatement("INSERT INTO Commandes Values(?, ?, ?, ?, ?)");
+            ajoutePlatStmt = conn.prepareStatement("INSERT INTO aPourPlats VALUES( ?, ?, ?, ?, ?)");
+            checkRestauAPlatStmt = conn.prepareStatement("SELECT numeroPlat FROM Plat" + 
+                                                            " WHERE mailRestaurant LIKE ?" +
+                                                            " AND nomPlat LIKE ?");
+            getPrixStmt = conn.prepareStatement("SELECT prix FROM Plat" +
+                                                " WHERE mailRestaurant LIKE ? AND idPlat LIKE ?");
+            getIdUserStmt = conn.prepareStatement("SELECT idUtilisateur FROM Client WHERE mailClient LIKE ?");
+            getPwdStmt = conn.prepareStatement("SELECT motDePasse FROM Client WHERE mailClient LIKE ?");
+            subcategoriesStmt = conn.prepareStatement("SELECT nomCategorieFille FROM aPourCategorieMere"
+                                                 +" WHERE nomCategorie LIKE ?");
+            resInCatStmt = conn.prepareStatement(
+                    " with recursive succesor as"
+                    +" (select nomCategorieFille as cat from aPourCategorieMere"
+                    +" where nomCategorie LIKE ?"
+                    +" union all"
+                    +" select nomCategorieFille from succesor, aPourCategorieMere"
+                    +" where succesor.cat = aPourCategorieMere.nomCategorie)"
+                    +" select distinct r.mailRestaurant, r.nomRestaurant, r.textPresentation, avg(ape.note) as noteMoy"
+                    +" from Restaurant r, aPourCategorie apc, succesor s, aPourEvaluation ape"
+                    +" where r.mailRestaurant = ape.mailRestaurant and"
+                    +" r.mailRestaurant = apc.mailRestaurant and "
+                    +"      (apc.nomCategorie = s.cat or"
+                    +"      apc.nomCategorie LIKE ?)"
+                    +" group by mailRestaurant order by noteMoy desc, r.nomRestaurant asc");
+            categoriesOfResStmt = conn.prepareStatement(
+            " with recursive ancestor as ("
+            +" select nomCategorie as cat from aPourCategorieMere"
+            +" where nomCategorieFille IN (SELECT nomCategorie from aPourCategorie where mailRestaurant like ?)"
+            +" union all"
+            +" select nomCategorie from ancestor, aPourCategorieMere"
+            +" where ancestor.cat = aPourCategorieMere.nomCategorieFille)"
+            +" select cat from ancestor"
+            +" UNION"
+            +" SELECT nomCategorie as cat from aPourCategorie where mailRestaurant like ?"
+            );
+            infoResStmt = conn.prepareStatement(
+            " SELECT *, avg(note) as noteMoy"
+            +" from Restaurant NATURAL JOIN aPourEvaluation "
+            +" where mailRestaurant LIKE ?");
+            scheduleDayStmt = conn.prepareStatement(
+            "select heureOuverture, heureFermeture"
+            +" from Horaires where jour LIKE ? and mailRestaurant LIKE ?"
+            +" order by heureOuverture asc");
+            getAllRestauStmt = conn.prepareStatement("SELECT nomRestaurant FROM Restaurant");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public Client(Connection conn) {
+        this.conn = conn;
+        prepareAllStatements();
+    }
+
+    public static void setIdUser(String mail){
+        try{
+            getIdUserStmt.setString(1, mail);
+            ResultSet res = getIdUserStmt.executeQuery();
+            if(res.next()){
+                idUser = res.getInt(1);
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void ajouteCommande(String date, String heure, String mailRestaurant, int prix){
+        try{
+            ajouteCommandeStmt.setString(1, date);
+            ajouteCommandeStmt.setString(2, heure);
+            ajouteCommandeStmt.setInt(3, idUser);
+            ajouteCommandeStmt.setString(4, mailRestaurant);
+            ajouteCommandeStmt.setInt(5, prix);
+            ajouteCommandeStmt.executeQuery();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String getMailRestaurant(String nomRestaurant){
+        try{
+            getMailRestaurantStmt.setString(1, nomRestaurant);
+            ResultSet res = getMailRestaurantStmt.executeQuery();
+            if(res.next()) return res.getString(1);
+            else return null;
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int checkRestauAPlat(String mailRestaurant, String nomPlat){
+        try{
+            checkRestauAPlatStmt.setString(1, mailRestaurant);
+            checkRestauAPlatStmt.setString(2, nomPlat);
+            ResultSet res = checkRestauAPlatStmt.executeQuery();
+            if(res.next()){
+                return res.getInt(1);
+            }
+            return 0;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static void ajoutePlat(String date, String heure, String mailRestaurant, int idPlat, int quantite){
+        try{
+            ajoutePlatStmt.setString(1, date);
+            ajoutePlatStmt.setString(2, heure);
+            ajoutePlatStmt.setInt(3, idUser);
+            ajoutePlatStmt.setString(4, mailRestaurant);
+            ajoutePlatStmt.setInt(5, idPlat);
+            ajoutePlatStmt.setInt(6, quantite);
+            ajoutePlatStmt.executeQuery();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static int getPrix(String mailRestaurant, int idPlat){
+        try{
+            getPrixStmt.setString(1, mailRestaurant);
+            getPrixStmt.setInt(2, idPlat);
+            ResultSet res = getPrixStmt.executeQuery();
+            if(res.next()) return res.getInt(1);
+            else return -1;
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public static boolean checkRestau(String nom){
+        try{
+            ResultSet res = getAllRestauStmt.executeQuery();
+            while(res.next()){
+                System.out.println("restau testé : " + res.getString(1));
+                if(res.getString(1).equals(nom))return true;
+            }
+            return false;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean essaiConnection(String mailClient, String motDePasse) {
+        try{
+            getPwdStmt.setString(1, mailClient);
+            ResultSet res = getPwdStmt.executeQuery();
+            if(res.next() && motDePasse.equals(res.getString(1))){
+                return true;
+            }
+            System.out.println("Mauvais mail ou mot de passe");
+            return false;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void browseCategories() {
+        String root = getRoot();
+        System.out.println(root);
+        System.out.println(getSubcategories(root));
+        System.out.println(getSubcategories(getSubcategories(root).get(0)));
+    }
+
+    public String getRoot() {
+        String root = "";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet res = stmt.executeQuery("SELECT nomCategorie FROM Categories"
+                              +" WHERE nomCategorie NOT IN "
+                              +" (SELECT nomCategorieFille FROM aPourCategorieMere)");
+            int count = 0;
+            while (res.next()) {
+                root = res.getString(1);
+                count++;
+            }
+            assert (count == 1);
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+        return root;
+    }
+    private List<String> getCategories(String restaurant) {
+        List<String> cats = new ArrayList<>();
+        try {
+            categoriesOfResStmt.setString(1, restaurant);
+            categoriesOfResStmt.setString(2, restaurant);
+            ResultSet res = categoriesOfResStmt.executeQuery();
+            while (res.next()) {
+                cats.add(res.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cats;
+    }
+    private void printSchedule(String restaurant) {
+        System.out.println("Schedule\n");
+        String[] days = {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"};
+        try {
+            ResultSet res = null;
+            for (String day : days) {
+                System.out.println(day);
+                scheduleDayStmt.setString(1, day);
+                scheduleDayStmt.setString(2, restaurant);
+                res = scheduleDayStmt.executeQuery();
+                while (res.next()) {
+                    System.out.println("Opening time: "+ res.getString("heureOuverture"));
+                    System.out.println("Closing time: "+ res.getString("heureFermeture"));
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+    }
+    /*
+     * Imprime tous les restaurants appartenant a la categorie category et
+     * tous ses filles.
+     * @param category
+     */
+    public void printRestaurant(String restaurant) {
+        try {
+            infoResStmt.setString(1, restaurant);
+            ResultSet res = infoResStmt.executeQuery();
+            while (res.next()) {
+                System.out.println("Email: " + res.getString("mailRestaurant"));
+                System.out.println("Name: " + res.getString("nomRestaurant"));
+                System.out.println("Description: " + res.getString("textPresentation"));
+                System.out.println("Restaurant address: " + res.getString("adresseRestaurant"));
+                System.out.println("Capacity: " + res.getString("nombrePlaces"));
+                System.out.println("Average mark: " + res.getString("noteMoy"));
+            }
+            System.out.print("Categories: ");
+            List<String> categories = getCategories(restaurant);
+            int i;
+            for (i = 0; i < categories.size()-1; i++) {
+                System.out.print(categories.get(i)+", ");
+            }
+            System.out.println(categories.get(i));
+            printSchedule(restaurant);
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+    }
+    public void printRestaurants(String category) {
+        try {
+            resInCatStmt.setString(1, category);
+            resInCatStmt.setString(2, category);
+            ResultSet res = resInCatStmt.executeQuery();
+            while (res.next()) {
+                System.out.println("Email: " + res.getString("mailRestaurant"));
+                System.out.println("Name: " + res.getString("nomRestaurant"));
+                System.out.println("Average mark: " + res.getString("noteMoy"));
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+    }
+    public List<String> getSubcategories(String category) {
+        List<String> subcategories = new ArrayList<>();
+        try {
+            subcategoriesStmt.setString(1, category);
+            ResultSet res = subcategoriesStmt.executeQuery();
+            while (res.next()) {
+                subcategories.add(res.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+        return subcategories;
+
     }
 
 }
